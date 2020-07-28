3f712f53553dcfc86104ea834321448561efede5
Michael Perel <michaelsethperel@gmail.com>
2020-07-28 03:32:08 +0000

    get token

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import { Application } from 'probot' // eslint-disable-line no-unused-vars
+import { Application, Context } from 'probot'
 
 const createScheduler = require('probot-scheduler')
 
-async function getLatestSHA(context: any, repo: string, owner: string, defaultBranchName: string): Promise<string> {
+async function getLatestSHA(context: Context, repo: string, owner: string, defaultBranchName: string): Promise<string> {
   const refs = await context.github.git.listRefs({
     owner: owner,
     repo: repo,
@@ -17,17 +17,24 @@ async function getLatestSHA(context: any, repo: string, owner: string, defaultBr
   return sha
 }
 
-async function getDefaultBranch(context: any, repo: string, owner: string): Promise<string> {
+async function createToken(context:Context, app: Application): Promise<string>{
+    // https://github.com/probot/probot/issues/1003
+    const auth = await app.auth()
+    const resp = await auth.apps.createInstallationToken({ installation_id: context.payload.installation.id })
+    return resp.data.token
+}
+
+async function getDefaultBranch(context: Context, repo: string, owner: string): Promise<string> {
   const { default_branch } = (await context.github.repos.get({ owner, repo })).data;
   return default_branch
 }
 
-function getRepo(context: any): string {
+function getRepo(context: Context): string {
   const {repo} = context.repo()
   return repo
 }
 
-function getOwner(context: any): string {
+function getOwner(context: Context): string {
   const {owner} = context.repo()
   return owner
 }
@@ -36,7 +43,7 @@ function getBranchName(): string {
   return "add-docker-lock9"
 }
 
-async function createBranch(context: any, repo:string, owner: string, branchName: string, sha: string) {
+async function createBranch(context: Context, repo:string, owner: string, branchName: string, sha: string) {
   const ref = `refs/heads/${branchName}`
   await context.github.git.createRef({
     owner: owner,
@@ -46,13 +53,17 @@ async function createBranch(context: any, repo:string, owner: string, branchName
   });
 }
 
-async function updateBranch(context: any, repo: string, owner: string, branchName: string) {
+async function updateBranch(context: Context, repo: string, owner: string, branchName: string) {
     const contents = await context.github.repos.getContents({
       owner: owner,
       repo: repo,
       ref: branchName,
       path: '.'
     });
+
+    if (!Array.isArray(contents.data)) {
+      throw new Error(`unexpected contents.data ${contents.data}`)
+    }
 
     let sha = undefined
     for (let d of contents.data) {
@@ -74,7 +85,7 @@ async function updateBranch(context: any, repo: string, owner: string, branchNam
     });
 }
 
-async function createPR(context: any, repo: string, owner: string, branchName: string) {
+async function createPR(context: Context, repo: string, owner: string, branchName: string) {
   const { default_branch } = (await context.github.repos.get({ owner, repo })).data;
 
   await context.github.pulls.create({
@@ -94,9 +105,12 @@ export = (app: Application) => {
   })
   
   // https://github.com/probot/scheduler
-  app.on('schedule.repository', async (context) => {
+  app.on('schedule.repository', async (context: Context) => {
     console.log("SCHEDULED EVENT")
 
+    const token = await createToken(context, app)
+    console.log("This is the token", token)
+  
     const repo = getRepo(context)
     const owner = getOwner(context)
     const defaultBranch = await getDefaultBranch(context, repo, owner)
@@ -115,8 +129,15 @@ export = (app: Application) => {
     catch (e) {
       console.log(e)
     }
+    console.log("CREATE BRANCH FINISHED")
 
-    await updateBranch(context, repo, owner, branchName)
+    try {
+      await updateBranch(context, repo, owner, branchName)
+    }
+    catch(e) {
+      console.log(e)
+    }
+    console.log("UPDATE BRANCH FINISHED")
   
     try {
       await createPR(context, repo, owner, branchName)
